a8e7013cfdbc3bf57621fbdf6e95f9fa75f4564b
2221057667 <2221057667@qq.com>
2018-08-24 18:04:11 +0800

    .

--- src/Main.java
+++ src/Main.java
@@ -1,20 +1,28 @@
-import java.util.*;
+import java.util.logging.*;
+import java.net.*;
 
 public class Main
 {
+	
+	
+	
 	public static void main(String[] args)
 	{
-		int i=0;
-		Main m=new Main();
-		System.out.println(i++ + +m.op(i));
-		System.out.println(i);
-	
+		Socket s=new Socket();
 		
-			}
-		public int op(int i){
-			System.out.println(i++);
-			return i;
+		while(true){
 			
+			
+			
+			
+			
+			
+		}
+		
+		
+		
+		
+		
 		}	
 			
 }
